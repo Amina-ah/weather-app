873556911614a71332c38f73051b41da97e36a5e
Amina-ah <79262078+Amina-ah@users.noreply.github.com>
2025-03-03 12:04:24 +0200

    created the function for the date , and added the humidity and wind

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 function refreshWeather(response) {
+
+    console/console.log(response);
+    
+    
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     temperatureElement.innerHTML=Math.round(temperature);
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.city;
+    let description = response.data.condition.description;
+    let descriptionElement= document.querySelector("#description");
+    descriptionElement.innerHTML =description ;
+    let humidity = `${response.data.temperature.humidity}%`;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML=humidity;
 
+    let wind = `${response.data.wind.speed} Km/h`;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML=wind;
+
+    let date = new Date();
+    let timeElement = document.querySelector("#date");
+    timeElement.innerHTML= formatDate(date);
+
+}
+
+function formatDate(date) {
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    if (minutes < 10){
+        minutes = `0${minutes}`
+    }
+    return `${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
