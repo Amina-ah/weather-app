ecf0beca1571ec5a16876a963e99f760a7577041
Amina-ah <79262078+Amina-ah@users.noreply.github.com>
2025-03-03 10:30:45 +0200

    search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML=Math.round(temperature);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+
+}
+
+function searchCity(city) {
+
+    let apiKey = "3b7851dt9259335e4ao904abf21d0f0b";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+    
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 
 function handleserachSubmit(event){
     event.preventDefault();
     let searchinput =document.querySelector("#form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchinput.value;
+    
+    searchCity (searchinput.value);
 
 }
 
 let SearchFormElement = document.querySelector("#search-form");
 SearchFormElement.addEventListener("submit",handleserachSubmit);
+
+searchCity("Khartoum");
